93e783c04d88e7e0236bce3c518311b52c0bfb2d
Cholpona17 <kuvvatbekovacholpon17@gmail.com>
2022-09-13 12:59:51 +0600

    commit

--- src/API/index.js
+++ src/API/index.js
@@ -2,7 +2,7 @@ import * as axios from "axios";
 
 
 const instanse = axios.create({
-    baseURL: "https://touristbish.herokuapp.com",
+    baseURL: "https://touristbish.herokuapp.com/",
     headers: {
         'Content-Type': 'application/json',
     }
@@ -10,16 +10,16 @@ const instanse = axios.create({
 
 export const ToursAPI = {
     getAllTours() {
-        return instanse.get("/main/location//").then(response => response.data)
+        return instanse.get("main/location//").then(response => response.data)
     },
     getDetailById(id) {
-        return instanse.get('/main/location//' + id + '/').then(response => response.data)
+        return instanse.get('main/location//' + id + '/').then(response => response.data)
     },
     getTransport() {
-        return instanse.get('/main/transport//').then(response => response.data)
+        return instanse.get('main/transport//').then(response => response.data)
     },
     getHotel() {
-        return instanse.get("/main/hotel//").then(response => response.data)
+        return instanse.get("main/hotel//").then(response => response.data)
     },
 
 }
